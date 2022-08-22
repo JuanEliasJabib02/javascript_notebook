de3de3f9ab0c6abfa935c4256ecde3061ae70b24
Juan Elias Jabib <juaneliasjabib02@gmail.com>
2022-08-22 11:11:09 -0500

    operators, booleans

---
+++ basics/05_operators.js
@@ -0,0 +1,29 @@
+const number1 = 10;
+const number2 = "10";
+const number3 = 30;
+
+
+//Equal to
+console.log(number1 == number2)
+
+// Equal to strict
+console.log(number1 === number2)
+
+// Not equal
+console.log(number1 != number2)
+
+// Not equal strict
+console.log(number1 !== number2)
+
+// greater than
+console.log(number1 > number3)
+
+// greater than or equal
+console.log(number1 >= number3)
+
+// less than
+console.log(number1 < number3)
+
+//less than or equal
+console.log(number1 <= number2)
+
---
+++ basics/06_booleans.js
@@ -0,0 +1,19 @@
+// the type of data booleans only has two values, true or false
+
+const auth = false;
+
+/* if(auth === true){
+    console.log("you can see netflix")
+}else{
+    console.log("get a subscription for use netflix")
+}
+ */
+
+// Good practice for compare booleans 
+
+if(auth){
+    console.log("you can see netflix")
+}else {
+    console.log("get a subscription for use netflix")
+}
+
