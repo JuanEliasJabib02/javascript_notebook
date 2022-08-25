3bc268e218b8a146e19c041207ff2def92ebea06
Juan Elias Jabib <juaneliasjabib02@gmail.com>
2022-08-25 14:17:53 -0500

    basic types ts

---
+++ typescript/01_basics/app.js
@@ -0,0 +1,19 @@
+// Core types
+/*
+    NUMBERS: 1, 5,3, -10  -> All numbers, no differentiation between integers and float
+
+    STRINGS: "hi",`Hi` 'Hi' -> All texts values
+
+    Booleans : True, False -> Just these two, no "truhty" or "falsy"
+ */
+var num = function (a, b, showResult, phrase) {
+    var result = a + b;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+    return result;
+};
+console.log(num(7, 2, true, "result is: "));
