a6f0594acb577cee59417c217f3e204d0acdf14e
Juan Elias Jabib <juaneliasjabib02@gmail.com>
2022-09-11 14:58:55 -0500

    data structures and algorithms section

---
+++ data structures & algorithms/01_big-o.js
@@ -0,0 +1,20 @@
+/* 
+
+  Big Os
+    
+    O(1) Constant - No loops
+
+    O(logN)Logarithmic - usually searching algorithms 
+                         have log n if they are sorted(Binary Search)
+    
+    O(n) Linear - For Loops, while loops, through n items
+
+    O(n log(n)) Log linear - usually sortings operations
+
+    O(^2) Quadratic - Every element in a collection needs to be compared to ever other
+                      element, two nested Loops
+
+    0(2) Exponencial - recursive algorithms that solves a problem of Size N
+    
+
+ */
