caf27444c3ff34965ac1d2c39dd9c9cf42b151a5
Juan Elias Jabib <juaneliasjabib02@gmail.com>
2022-09-11 18:28:40 -0500

    cheat sheep finish big o

--- data structures & algorithms/01_big-o.js
+++ data structures & algorithms/01_big-o.js
@@ -14,7 +14,42 @@
     O(^2) Quadratic - Every element in a collection needs to be compared to ever other
                       element, two nested Loops
 
-    0(2) Exponencial - recursive algorithms that solves a problem of Size N
+    O(2^n) Exponencial - recursive algorithms that solves a problem of Size 
     
+    O(n!) Factorial - you are adding a loop for every element
 
+    Iterating through half a collection is still O(n)
+
+    two separte collections: O( a * b)
+
+
+    What can cause time in a function?
+
+    Operation(+,-,*,/)
+    Comparisons(<,>,==)
+    Looping(for,while)
+    outside function call(function())
+
+
+    RULE BOOOK
+
+    Rule 1: always worst case
+    Rule 2: Remove constants
+    Rule 3:
+      -Different inputs should have different variables O(a+b)
+      -A and B arrays nested would be : O (a*b)
+      -for steps in order
+      - for nested steps
+
+
+    Rule 4: Drop-Non-dominant terms
+
+
+    WHAT CAUSES SPACE COMPLEXITY
+    
+    - Variables
+    - Data structures
+    - Function Call
+    - Allocations
+  
  */
