d142aacf1f39fb41f217d32ef5fa6bb06bd0f1f6
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2022-10-25 00:10:56 -0500

    study

---
+++ basics/09_Scope/1.3_clousures.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Clousures:
+ * 
+ * Are the nested functions that remenber the set of variables
+ * to which they could have access, no matters how much
+ * they are invoked in others places(other scope)
+ */
+
+const Counter = function makeCounter() {
+  let _counter = 0;
+  /* The function and the variable are united by an inseparable link
+    In this case counter and the method increment
+   */
+}
