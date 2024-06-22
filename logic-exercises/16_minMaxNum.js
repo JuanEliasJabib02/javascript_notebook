560e4be3d1af4f114f91299b4ef2e7161b7d3c18
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2024-06-21 20:51:57 -0500

    exercise completed

--- logic-exercises/16_minMaxNum.js
+++ logic-exercises/16_minMaxNum.js
@@ -1,40 +1,13 @@
+function miniMaxSum(arr) {
+  // Write your code here
+  /* -1  */
+  const minSumArray = arr.slice(0, -1)
+  const maxSumArray = arr.slice(1)
 
-// Mini- Max Sum HackerRam
-const minMaxNum = (arr) =>{
-
-  const min = [];
-  const max = [];
-
-
-  for(var i = 0; i < arr.length - 1; i++){
-    min.push(arr[i])
-  }
-
-  for(var j = 1; j < arr.length; j++){
-
-    max.push(arr[j])
-
-  }
-
-  let minSum = 0;
-  let maxSum = 0;
-
-  for (var number of min) {
-    minSum += number
-  }
-
-  for (var number of max) {
-    maxSum += number
-  }
-
-  console.log(minSum, maxSum)
-
-
+  const minSum = minSumArray.reduce((acc, currentValue) => acc + currentValue)
+  const maxSum = maxSumArray.reduce((acc, currentValue) => acc + currentValue)
+    
+  return {minSum,maxSum}
 }
 
-
-minMaxNum([7,69,2,221,8974])
-
-
-// MIN : 1 + 2 + 3 + 4 = 10
-// MAX : 2 + 3 + 4 + 5 = 14
+console.log(miniMaxSum([1,2,3,4,5]))
